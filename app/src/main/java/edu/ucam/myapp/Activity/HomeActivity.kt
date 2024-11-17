14138c254ca26f42e14921435412d61b770befc7
Arturo <codealgr@gmail.com>
2024-11-17 19:38:53 +0100

    Añadir sensor temperatura

--- app/src/main/java/edu/ucam/myapp/Activity/HomeActivity.kt
+++ app/src/main/java/edu/ucam/myapp/Activity/HomeActivity.kt
@@ -1,26 +1,39 @@
 package edu.ucam.myapp.Activity
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import edu.ucam.myapp.Adapter.CategoryAdapter
 import edu.ucam.myapp.Adapter.OfferAdapter
 import edu.ucam.myapp.Adapter.PopularAdpater
 import edu.ucam.myapp.ViewModel.MainViewModel
 import edu.ucam.myapp.databinding.ActivityHomeBinding
+import android.hardware.SensorManager
+import android.content.Context
+import edu.ucam.myapp.R
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : BaseActivity(), SensorEventListener {
     private lateinit var binding: ActivityHomeBinding
     private val viewModel = MainViewModel()
+    private lateinit var sensorManager: SensorManager
+    private var temperatureSensor: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        initCategory()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+
+        if (temperatureSensor == null) {
+            binding.temperatureText.text = "Sensor no disponible"
+        }
+
         initPopular()
         initOffer()
 
@@ -56,25 +69,46 @@ class HomeActivity : BaseActivity() {
         viewModel.loadPopular()
     }
 
-//    private fun initCategory() {
-//        binding.recyclerViewCategory.layoutManager = LinearLayoutManager(
-//            this@HomeActivity,
-//            LinearLayoutManager.HORIZONTAL,
-//            false
-//        )
-//        binding.progressBarCategory.visibility = View.VISIBLE
-//
-//        viewModel.category.observe(this, Observer { categories ->
-//            if (categories.isNotEmpty()) {
-//                binding.recyclerViewCategory.adapter = CategoryAdapter(categories)
-//            } else {
-//                // Opcional: muestra un mensaje o maneja el caso de lista vacía
-//                Log.d("HomeActivity", "La lista de categorías está vacía")
-//            }
-//            binding.progressBarCategory.visibility = View.GONE
-//        })
-//
-//        viewModel.loadCategory()
-//    }
+    override fun onResume() {
+        super.onResume()
+        temperatureSensor?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
 
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null && event.sensor.type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+//            val temperature = event.values[0]
+            val temperature = 20.0
+            Log.d("HomeActivity", "Valor del sensor de temperatura: $temperature")
+
+            if (temperature in -50.0..50.0) {
+                binding.temperatureText.text = "Temperatura: ${temperature}°C"
+
+                when {
+                    temperature < 15 -> {
+                        binding.coffeeIcon.setImageResource(R.drawable.coffee)
+                    }
+                    temperature in 15.0..25.0 -> {
+                        binding.coffeeIcon.setImageResource(R.drawable.coffee)
+                    }
+                    temperature > 25 -> {
+                        binding.coffeeIcon.setImageResource(R.drawable.coffee)
+                    }
+                }
+            } else {
+                binding.temperatureText.text = "Temperatura no disponible"
+                binding.coffeeIcon.setImageResource(R.drawable.coffee)
+            }
+        }
+    }
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
 }
--- app/src/main/java/edu/ucam/myapp/ViewModel/MainViewModel.kt
+++ app/src/main/java/edu/ucam/myapp/ViewModel/MainViewModel.kt
@@ -8,43 +8,17 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import edu.ucam.myapp.Model.CategoryModel
 import edu.ucam.myapp.Model.ItemsModel
 
 
 class MainViewModel : ViewModel() {
     private val firebaseDatabase = FirebaseDatabase.getInstance()
 
-    private val _categoryList = MutableLiveData<MutableList<CategoryModel>>()
     private val _popularList = MutableLiveData<MutableList<ItemsModel>>()
     private val _offerList = MutableLiveData<MutableList<ItemsModel>>()
 
-    val category: LiveData<MutableList<CategoryModel>> = _categoryList
     val popular: LiveData<MutableList<ItemsModel>> = _popularList
     val offer: LiveData<MutableList<ItemsModel>> = _offerList
-
-    fun loadCategory() {
-        val ref = firebaseDatabase.getReference("Category")
-        ref.addValueEventListener(object :ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val lists = mutableListOf<CategoryModel>()
-
-                for (snap in snapshot.children){
-                    val category = snap.getValue(CategoryModel::class.java)
-                    if (category != null){
-                        lists.add(category)
-                    }
-                }
-
-                _categoryList.value = lists
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
-    }
 
     fun loadPopular() {
         val ref = firebaseDatabase.getReference("Items")
